bbb5ea2bd7bfb796b8107a4b4f5888dd39d6a4f2
Slawomir <slawomir@tempity.com>
2013-12-03 21:04:39 +0100

    t

---
+++ inpost/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inpost::Application.config.secret_token = 'e72b0890e8a7460371a1681eb4448ed485c62593f9095efe55c43ec12937da7ba629f038154c96695333ad77c6334d68f99e97bbd23a10e7bb4bfc2bbca7ec20'
