383335e8a379a6b35f460535d62fef58cb828d31
Slawomir <slawomir@tempity.com>
2013-12-03 23:01:06 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inpost::Application.config.secret_key_base = 'f1dcf38d32451a28946e66e0b96aff78a1c3b42617dac1a2a72763aa9774ce37926b8fdbbca83460dc4d7bae2856d742eaf7522d5479332a234f73b738bb4447'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Inpost::Application.config.session_store :cookie_store, key: '_inpost_session'
